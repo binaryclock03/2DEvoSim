2f9b703326abb42be80e073cf195b537e359295c
binaryclock03 <binaryclock03@gmail.com>
2022-05-28 00:54:36 -0400

    .

--- netObjects.py
+++ netObjects.py
@@ -1,64 +1,83 @@
 from genome import Genome
 from util import clamp
+import random
+import math
 
 class Neuron():
     def __init__(self):
-        self._address:int
-        self.strength:float
+        self._address:int = 0
+        self.value:float = 0
+        self.incoming:list = []
     
     def set_address(self, address:int):
         self._address = clamp(address, 0, 255)
 
     def get_address(self):
         return self.address
-    
+
+    def activate(self, action="None"):
+        pass
+
 class InterNeuron(Neuron):
     def __init__(self):
         super().__init__(self)
-    
-    def calc_incoming():
-        pass
 
-class IONeuron(Neuron):
-    def __init__(self):
-        super().__init__(self)
-        self.function: function
+    def activate(self, action="None"):
+        if action == "Sum":
+            self.value = math.tanh(sum(self.incoming))
 
-class SensorNeuron(IONeuron):
+class SensorNeuron(Neuron):
     def __init__(self):
         super().__init__(self)
     
-    def read_sensor(self):
-        self.strength = self.function
+    def activate(self, action="None"):
+        if action == "Sensor":
+            self.value = self.function
 
-class ActionNeuron(IONeuron, InterNeuron):
+class ActionNeuron(Neuron):
     def __init__(self):
         super().__init__(self)
     
-    def preform_action(self):
-        self.function()
+    def activate(self, action="None"):
+        if action == "Sum":
+            self.value = math.tanh(sum(self.incoming))
+        if action == "Action" and random.random()<self.value:
+            self.function()
 
 class Connection():
     def __init__(self, gene):
         self.adr_a = int(gene[0:2], base=16)
         self.adr_b = int(gene[2:4], base=16)
-        self._strength = int(gene[4:8], base=16)
-        
+        self.strength = int(gene[4:8], base=16)
 
 class NeuralNet():
     def __init__(self):
-        self.sensorNeurons:list = []
-        self.actionNeurons:list = []
+        self.neurons:list = []
         self.connections:list = []
     
     def build_net(self, genome:Genome):
         for gene in genome.genes:
             self.connections.append(Connection(gene))
+        
+        for index in range(383):
+            self.neurons.append(Neuron())
 
-    def activate_sensors(self):
-        for sensorNeuron in self.sensorNeurons:
-            sensorNeuron.read_sensor()
-    
-    def activate_actions(self):
-        for actionNeuron in self.actionNeurons:
-            actionNeuron.preform_action()
+    def activate(self):
+        for neuron in self.neurons:
+            neuron.activate("Sensor")
+
+        for connection in self.connections:
+            adr_a = connection.adr_a
+            adr_b = connection.adr_b
+            if adr_b < 128:
+                adr_b += 256
+
+            value = self.neurons[adr_a].value
+            value = value * connection.strength
+            self.neurons[adr_b].incoming.append(value)
+            
+        for neuron in self.neurons:
+            neuron.activate("Sum")
+
+        for neuron in self.neurons:
+            neuron.activate("Activate")
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import netObjects as nt
 genome = Genome(4)
 brain = nt.NeuralNet()
 brain.build_net(genome)
-print(brain.connections)
+brain.activate()
