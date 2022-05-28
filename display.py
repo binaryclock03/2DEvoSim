b46c9959abfdaac9321c9abe4ed820e6956c651f
binaryclock03 <binaryclock03@gmail.com>
2022-05-28 15:50:15 -0400

    drawing now supports self connections

--- display.py
+++ display.py
@@ -12,14 +12,31 @@ def run(brain, max_input = 128, max_output = 128, max_inter = 128):
     for index in range(max_inter):
         x = random.randrange(10,1790)
         y = random.randrange(300,500)
-
         inter.update({index+128:(x,y)})
+
+    conn = []
+    for index in range(256):
+        color = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
+        conn.append(color)
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 return False
+            if event.type == pg.KEYDOWN:
+                keydown = pg.key.get_pressed()
+                if keydown[pg.K_1]:
+                    inter = {}
+                    for index in range(max_inter):
+                        x = random.randrange(10,1790)
+                        y = random.randrange(300,500)
+                        inter.update({index+128:(x,y)})
+                if keydown[pg.K_2]:
+                    conn = []
+                    for index in range(256):
+                        color = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
+                        conn.append(color)
 
         display.fill((255,255,255))
         font = pg.font.Font('freesansbold.ttf', 10)
@@ -40,6 +57,7 @@ def run(brain, max_input = 128, max_output = 128, max_inter = 128):
                 color = (0,255,0)
             pg.draw.circle(display, color, (x,600), 5)
 
+        i = 0
         for connection in brain.connections:
             adr_a = connection.adr_a
             adr_b = connection.adr_b
@@ -56,17 +74,21 @@ def run(brain, max_input = 128, max_output = 128, max_inter = 128):
             else:
                 xa, ya = inter.get(adr_a)
 
-            color = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
+            color = conn[i]
             pg.draw.line(display, color, (xa,ya), (xb,yb), width=5)
             dx = xb-xa
             dy = yb-ya
             length = sqrt(dx**2 + dy**2)
-            dx = dx/length
-            dy = dy/length
-            arrowPos = 0.6
-            arrowWidth = 10
-            arrowLength = 15
-            pg.draw.polygon(display, color, [(xa+dx*length*arrowPos+dx*arrowLength,ya+dy*length*arrowPos+dy*arrowLength), (xa+dx*length*arrowPos+dy*arrowWidth,ya+dy*length*arrowPos+dx*-arrowWidth), (xa+dx*length*arrowPos+dy*-arrowWidth,ya+dy*length*arrowPos+dx*arrowWidth)])
+            if length != 0:
+                dx = dx/length
+                dy = dy/length
+                arrowPos = 0.6
+                arrowWidth = 10
+                arrowLength = 15
+                pg.draw.polygon(display, color, [(xa+dx*length*arrowPos+dx*arrowLength,ya+dy*length*arrowPos+dy*arrowLength), (xa+dx*length*arrowPos+dy*arrowWidth,ya+dy*length*arrowPos+dx*-arrowWidth), (xa+dx*length*arrowPos+dy*-arrowWidth,ya+dy*length*arrowPos+dx*arrowWidth)])
+            else:
+                pg.draw.circle(display, color, (xa+7,ya-7), 12, width=4)
+            i += 1
 
         for index in range(128, max_inter+128):
             coords = inter.get(index)
