d282b4246d897e55c69ec87310057727f17e404f
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-28 00:53:01 -0400

    improved genome generation algorithm

--- genome.py
+++ genome.py
@@ -5,19 +5,40 @@ import random
 Genome = NewType("Genome",object)
 
 class Genome():
-    def __init__(self,number_of_genes:int,genes:List = None,) -> None:
+    def __init__(self,number_of_genes:int,genes:List = None,max_input:int = 0,max_output:int = 0,max_inter:int = 0) -> None:
         
         self.number_of_genes = number_of_genes
         
         self.genes = []
         
+        self.max_input = max_input
+        self.max_output = max_output
+        self.max_inter = max_inter
+        
         if genes != None:
             self.genes = genes
         else:
-            for x in range(number_of_genes):
-                gene = hex(random.randint(0,(16**8)-1))
-                self.genes.append(gene.replace("0x", ""))
-        
+            if max_input + max_inter + max_inter == 0:
+                for x in range(number_of_genes):
+                    gene = hex(random.randint(0,(16**8) - 1))
+                    self.genes.append(gene.replace("0x", ""))
+            else:
+                for x in range(number_of_genes):
+                    if max_input == 0:
+                        max_input = 128
+                    if max_output == 0:
+                        max_output = 128
+                    if max_inter == 0:
+                        max_inter = 256
+                        
+                    strength = hex(random.randint(0,(16**4) - 1)).replace("0x", "")
+                    possible_in_adr = list(range(self.max_input+1)) + list(range(128,self.max_inter+1))
+                    in_adr = hex(random.choice(possible_in_adr)).replace("0x", "").zfill(2)
+                    possible_out_adr = list(range(self.max_input+1)) + list(range(128,self.max_inter+1))
+                    out_adr = hex(random.choice(possible_out_adr)).replace("0x", "").zfill(2)
+                    gene = in_adr + strength + out_adr
+                    self.genes.append(gene)
+                   
     def __str__(self) -> str:
         output_str = ''
         for gene in self.genes:
@@ -45,3 +66,5 @@ class Genome():
         new_genes = self.genes
         new_genes[index] = new_gene
         return Genome(self.number_of_genes,new_genes)
+print(Genome(4))
+print(Genome(4,max_input=4,max_output=4,max_inter=256))
