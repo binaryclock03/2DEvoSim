3707f3a7b0d3d6fa7b43f6e74283834be464c26a
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-29 19:08:04 -0400

    Added methods to prepare new generations

--- population.py
+++ population.py
@@ -23,19 +23,36 @@ class Population():
         self.genomes = []
     
     def generate_genomes(self,genomes:int,genes:int,max_input_index:int = None,max_output_index:int = None,max_inter_index:int = None) -> None:
+        self.amount_of_genomes = genomes
         for x in range(genomes):
             self.genomes.append(genome.Genome(genes,max_input_index,max_output_index,max_inter_index))
     
     def save_generation(self) -> None:
         file = open('Populations\\' + self.name + '.csv','a')
-        csv_writer = csv.writer(file, lineterminator='\n')
+        csv_writer = csv.writer(file)
         csv_writer.writerow(self.genomes)
 
     def load_generation(self,generation_number:int) -> None:
         file = open('Populations\\' + self.name + '.csv','r')
-        csv_reader = csv.reader(file, lineterminator='\n')
+        csv_reader = csv.reader(file)
         self.genomes = csv_reader[generation_number]
         
     def mutate_population(self):
         for genome in self.genomes:
             genome.mutate_gene()
+    
+    def reproduce(self,mutation_rate:float):
+        if len(self.genomes) > self.amount_of_genomes:
+            amount_to_fill = self.amount_of_genomes - len(self.genomes)
+            for i in range(amount_to_fill):
+                self.genomes.append(random.choice(self.genomes).copy())
+        for genome in self.genomes:
+            genome.mutate_genome(mutation_rate)
+        
+    def new_generation(self, survivors:list,mutation_rate:int):
+        self.genomes = survivors
+        self.generation += 1
+        #create genome object for each survivor
+        self.reproduce(mutation_rate)
+        
+        self.save_generation()
