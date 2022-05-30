f879b68b4fb21e1161159d7dbf4d3ab984168ecc
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-29 23:47:29 -0400

    Test simulation

--- functions.py
+++ functions.py
@@ -1,12 +1,26 @@
 import netObjects as nt
+import simulation as sim
+import util
 
 #sensors
-def testSensor(creature, simulation):
-    return 1
+# def testSensor(creature, simulation):
+#     return 1
+
+def get_y_pos(creature, simulation):
+    y_pos = simulation.get_creature_position(creature.id)[1]
+    y_bound = simulation.grid_bounds[1]
+    output = util.scale(y_pos,0,y_bound,-1,1)
+    return output
+
+def get_x_pos(creature, simulation):
+    x_pos = simulation.get_creature_position(creature.id)[0]
+    x_bound = simulation.grid_bounds[0]
+    output = util.scale(x_pos,0,x_bound,-1,1)
+    return output
 
 #actions
-def testAction(self, creature, simulation):
-    print(f"TEST ACTION")
+# def testAction(self, creature, simulation):
+#     print(f"TEST ACTION")
 
 def move_x(self, creature, simulation):
     if self.value < 0:
@@ -14,8 +28,17 @@ def move_x(self, creature, simulation):
     else:
         simulation.move_creature(creature.id, 1, 0)
 
+def move_y(self, creature, simulation):
+    if self.value < 0:
+        simulation.move_creature(creature.id, 0, -1)
+    else:
+        simulation.move_creature(creature.id, 0, 1)
+
 sensor_neurons = []
-sensor_neurons.append(nt.SensorNeuron(testSensor))
+#sensor_neurons.append(nt.SensorNeuron(testSensor))
+sensor_neurons.append(nt.SensorNeuron(get_y_pos))
+sensor_neurons.append(nt.SensorNeuron(get_x_pos))
 
 action_neurons = []
 action_neurons.append(nt.ActionNeuron(move_x, move_x))
+action_neurons.append(nt.ActionNeuron(move_y, move_y))
--- run.py
+++ run.py
@@ -2,36 +2,50 @@ import time
 from genome import Genome
 import simulation as sim
 import functions as f
-import display
+#import display
+import population as pop
 
 if __name__ == "__main__":
-    simulation = sim.Simulation()
+
 
     sensor_neurons = f.sensor_neurons
     action_neurons = f.action_neurons
-
-    for i in range(1,11):
-        genome = Genome(1, genes=["0000ffff"])
-        creature = sim.Creature(i)
-        creature.build_from_genome(genome)
-        creature.add_io_neurons(sensor_neurons, action_neurons)
-        creature.finalize()
-        simulation.add_to_sim(creature)
     
-    print("before")
-    print(simulation.get_creature_position(1))
-    print(simulation.get_creature_position(2))
+    #Initialize Population
+    population = pop.Population("6")
+    population.generate_genomes(250,4,2,2,1)
+    population.save_generation()
     
-    for i in range(2):
-        simulation.simulate()
+    for gen in range(1000):
+        start = time.time()
+        simulation = sim.Simulation()
+        
+        for i in range(250):
+            creature = sim.Creature(i+1)
+            creature.build_from_genome(population.genomes[i])
+            creature.add_io_neurons(sensor_neurons, action_neurons)
+            creature.finalize()
+            simulation.add_to_sim(creature)
+
+        for x in range(200):
+            simulation.simulate()
+
+        survivors = []
+
+        for creep in simulation.creatures:
+            if simulation.get_creature_position(creep.id)[1] < 16:
+                survivors.append(creep.genome)
+        
+        print(len(survivors))
+        population.new_generation(survivors,0.01)
+        
+        end = time.time()
+        print(f"time elapsed {end-start}")
+        
+        print("Generation: " +  str(gen))
     
-    print("after")
-    print(simulation.get_creature_position(1))
-    print(simulation.get_creature_position(2))
-
-
     # start = time.time()
-    # brain.activate(None)
+    # 
     # end = time.time()
     # print(f"time elapsed {end-start}")
 
