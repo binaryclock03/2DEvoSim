03ab6b63a6d5745b13d7549d1ec2b35c739f9d84
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-05-30 12:13:51 -0400

    Added functions to function.py and reorganized it

--- functions.py
+++ functions.py
@@ -11,22 +11,16 @@ def always_on(creature, simulation):
 def always_off(creature, simulation):
     return 0
 
-def get_y_pos(creature, simulation):
-    y_pos = simulation.get_creature_position(creature.id)[1]
-    y_bound = simulation.grid_bounds[1]
-    output = util.scale(y_pos,0,y_bound,-1,1)
-    return output
-
 def get_x_pos(creature, simulation):
     x_pos = simulation.get_creature_position(creature.id)[0]
     x_bound = simulation.grid_bounds[0]
     output = util.scale(x_pos,0,x_bound,-1,1)
     return output
 
-def get_y_dist(creature,simulation):
+def get_y_pos(creature, simulation):
     y_pos = simulation.get_creature_position(creature.id)[1]
     y_bound = simulation.grid_bounds[1]
-    output = util.scale(y_pos,0,y_bound,0,1)
+    output = util.scale(y_pos,0,y_bound,-1,1)
     return output
 
 def get_x_dist(creature,simulation):
@@ -35,56 +29,88 @@ def get_x_dist(creature,simulation):
     output = util.scale(x_pos,0,x_bound,0,1)
     return output
 
+def get_y_dist(creature,simulation):
+    y_pos = simulation.get_creature_position(creature.id)[1]
+    y_bound = simulation.grid_bounds[1]
+    output = util.scale(y_pos,0,y_bound,0,1)
+    return output
+
 def oscillator(creature, simulation):
     return math.sin(simulation.tick*math.pi*0.25)
     
 #actions
-def move_random(self, creature, simulation):
-    x = random.randrange(-1,1)
-    y = random.randrange(-1,1)
-    simulation.move_creature(creature.id, x, y)
 
 def move_x(self, creature, simulation):
-    if self.value < 0:
-        simulation.move_creature(creature.id, -1, 0)
-        creature.dir = 2
-    else:
-        simulation.move_creature(creature.id, 1, 0)
-        creature.dir = 0
+    if creature.delta[0] == 0:
+        if self.value < 0:
+            delta = simulation.move_creature(creature.id, -1, 0)
+            if delta != [0,0]:
+                creature.dir[0] = -1
+        else:
+            delta = simulation.move_creature(creature.id, 1, 0)
+            if delta != [0,0]:
+                creature.dir[0] = 1
+        creature.delta = delta
 
 def move_y(self, creature, simulation):
-    if self.value < 0:
-        simulation.move_creature(creature.id, 0, -1)
-        creature.dir = 3
-    else:
-        simulation.move_creature(creature.id, 0, 1)
-        creature.dir = 1
+    if creature.delta[1] == 0:
+        if self.value < 0:
+            delta = simulation.move_creature(creature.id, 0, -1)
+            if delta != [0,0]:
+                creature.dir[1] = -1
+        else:
+            delta = simulation.move_creature(creature.id, 0, 1)
+            if delta != [0,0]:
+                creature.dir[1] = 1
+        creature.delta = delta
+        
+def move_fwd(self,creature,simulation):
+    if creature.delta == [0,0]:
+        dir = creature.dir
+        if self.value < 0:
+            delta = simulation.move_creature(creature.id, -1*dir[0], -1*dir[1])
+        else:
+            delta = simulation.move_creature(creature.id, dir[0], dir[1])
+        creature.delta = delta
+    
+def move_rnd(self,creature,simulation):
+    delta_target = [random.randint(-1,1),random.randint(-1,1)]
+    if creature.delta == [0,0]:
+        if self.value < 0:
+            delta = simulation.move_creature(creature.id, -1*delta_target[0], -1*delta_target[1])
+            creature.dir = delta
+        else:
+            delta = simulation.move_creature(creature.id, delta_target[0], delta_target[1])
+            creature.dir = delta
+        creature.delta = delta
 
 
 
 sensor_neurons = []
 sensor_neurons.append(nt.SensorNeuron(always_on))
 sensor_neurons.append(nt.SensorNeuron(always_off))
-sensor_neurons.append(nt.SensorNeuron(get_y_pos))
 sensor_neurons.append(nt.SensorNeuron(get_x_pos))
-sensor_neurons.append(nt.SensorNeuron(get_y_dist))
+sensor_neurons.append(nt.SensorNeuron(get_y_pos))
 sensor_neurons.append(nt.SensorNeuron(get_x_dist))
+sensor_neurons.append(nt.SensorNeuron(get_y_dist))
 sensor_neurons.append(nt.SensorNeuron(oscillator))
 
 action_neurons = []
-action_neurons.append(nt.ActionNeuron(move_random, move_random))
 action_neurons.append(nt.ActionNeuron(move_x, move_x))
 action_neurons.append(nt.ActionNeuron(move_y, move_y))
+action_neurons.append(nt.ActionNeuron(move_fwd, move_fwd))
+action_neurons.append(nt.ActionNeuron(move_rnd, move_rnd))
 
 neuron_name_dict = {
     0:"always on",
     1:"always off",
-    2:"get y pos",
-    3:"get x pos",
-    4:"get y dist",
-    5:"get x dist",
+    2:"get x pos",
+    3:"get y pos",
+    4:"get x dist",
+    5:"get y dist",
     6:"oscillator",
-    256:"move rand",
-    257:"move x",
-    258:"move y"
+    256:"move x",
+    257:"move y",
+    258:"move fwd",
+    259:"move rnd"
 }
